a4f5453c767e5f956d1aeb9e8fc35b72781bd69f
Mangb0 <whdudghks45629@gmail.com>
2024-03-21 23:36:47 +0900

    a 페이지 추가

---
+++ src/app/a/page.tsx
@@ -0,0 +1,12 @@
+export default function PageA() {
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-col justify-center items-center h-52 gap-4">
+        <p className="text-4xl font-bold">Trusted by creators worldwide</p>
+        <p className="text-lg text-gray-500">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+      </div>
+    </div>
+  )
+}
