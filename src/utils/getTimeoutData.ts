0d766bd9c3e96c5dbb6ba563b208387c50910dc9
Mangb0 <whdudghks45629@gmail.com>
2024-03-23 00:28:08 +0900

    utils getTimeoutData 수정

--- src/utils/getTimeoutData.ts
+++ src/utils/getTimeoutData.ts
@@ -1,10 +1,11 @@
-export async function getTimeoutData() {
+export const getTimeoutData = async () => {
+  const response = await fetch('http://localhost:3000/data.json', {
+    cache: 'no-store',
+  })
+  const data = await response.json()
   return new Promise((resolve) => {
-    setTimeout(resolve, 3000, {
-      creators: 8000,
-      platformFeePercent: 3,
-      uptimePercent: 99.9,
-      paidCreators: '70M',
-    })
+    setTimeout(() => {
+      resolve(data)
+    }, 3000)
   })
 }
