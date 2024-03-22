75cc2bf32ae5ea83db67d4a39ac4686dfacb7eac
Mangb0 <whdudghks45629@gmail.com>
2024-03-23 00:42:32 +0900

    b 페이지 추가 및 SSR 적용

---
+++ src/app/b/page.tsx
@@ -0,0 +1,34 @@
+import { getTimeoutData } from '@/utils/getTimeoutData'
+
+interface Data {
+  creaters: number
+  platformFeePercent: number
+  uptimePercent: number
+  paidCreaters: string
+}
+
+export default async function PageB() {
+  const data = (await getTimeoutData()) as Data
+  const { creaters, platformFeePercent, uptimePercent, paidCreaters } = data
+
+  return (
+    <div className="flex flex-grow justify-center items-center w-full h-40 font-bold">
+      <div className="flex flex-col flex-1 items-center">
+        <p className="text-2xl">{creaters}+</p>
+        <p className="text-sm text-gray-500">Creators on the platform</p>
+      </div>
+      <div className="flex flex-col flex-1 items-center">
+        <p className="text-2xl">{platformFeePercent}%</p>
+        <p className="text-sm text-gray-500">Flat pllatform fee</p>
+      </div>
+      <div className="flex flex-col flex-1 items-center">
+        <p className="text-2xl">{uptimePercent}%</p>
+        <p className="text-sm text-gray-500">Uptime guarantee</p>
+      </div>
+      <div className="flex flex-col flex-1 items-center">
+        <p className="text-2xl font-bold">${paidCreaters}</p>
+        <p className="text-sm text-gray-500">Paid out to creators</p>
+      </div>
+    </div>
+  )
+}
