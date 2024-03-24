84723759a95c7236579b8dd94f07be6a0aef6c0c
Mangb0 <whdudghks45629@gmail.com>
2024-03-24 15:08:18 +0900

    c 페이지 수정

--- src/app/c/page.tsx
+++ src/app/c/page.tsx
@@ -1,57 +1,13 @@
 'use client'
 
 import Title from '@/components/Title'
-import { Data } from '@/types/types'
-import { useEffect, useState } from 'react'
+import ClientSide from '@/components/clientSide'
 
 export default function PageC() {
-  const [data, setData] = useState<Data>({
-    creaters: 0,
-    platformFeePercent: 0,
-    uptimePercent: 0,
-    paidCreaters: '',
-  })
-  const [isLoading, setIsLoading] = useState<boolean>(true)
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('/api/hello')
-      if (!response.ok) {
-        throw new Error('Network response was not ok')
-      }
-      const data = await response.json()
-      setData(data)
-      setIsLoading(false)
-    }
-
-    fetchData()
-  }, [])
-
   return (
     <div className="flex flex-col">
       <Title />
-      {!isLoading ? (
-        <div className="flex flex-grow justify-center items-center w-full h-40 font-bold">
-          <div className="flex flex-col flex-1 items-center">
-            <p className="text-2xl">{data.creaters}+</p>
-            <p className="text-sm text-gray-500">Creators on the platform</p>
-          </div>
-          <div className="flex flex-col flex-1 items-center">
-            <p className="text-2xl">{data.platformFeePercent}%</p>
-            <p className="text-sm text-gray-500">Flat pllatform fee</p>
-          </div>
-          <div className="flex flex-col flex-1 items-center">
-            <p className="text-2xl">{data.uptimePercent}%</p>
-            <p className="text-sm text-gray-500">Uptime guarantee</p>
-          </div>
-          <div className="flex flex-col flex-1 items-center">
-            <p className="text-2xl font-bold">${data.paidCreaters}</p>
-            <p className="text-sm text-gray-500">Paid out to creators</p>
-          </div>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+      <ClientSide />
     </div>
   )
 }
